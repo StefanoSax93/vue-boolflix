0cdd92a0e707be5d5a6156dd0eff8c0b23a7ee3f
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-29 12:41:00 +0200

    aggiunte provvisorie

--- src/components/StampCard.vue
+++ src/components/StampCard.vue
@@ -1,28 +1,18 @@
 <template>
-    <div>
-        <!-- se è un movie -->
-        <div v-if="movie.title">
-            <ul class="list-unstyled">
-                <li class="mb-3">
-                    <div>{{`Titolo: ${movie.title}`}}</div>
-                    <div>{{`Titolo originale: ${movie.original_title}`}}</div>
-                    <div>Lingua: <lang-flag :iso="movie.original_language" class="rounded"/></div>
-                    <div>{{`Voto: ${movie.vote_average}`}}</div>
-                </li>
-            </ul>
+        <div>
+            <div class="mb-3 w-100 position-relative poster">
+                    <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="" @error="onImgError">
+                    <div class="card-overlay text-white p-3">
+                        <div v-if="movie.title"><strong>Titolo:</strong> {{movie.title}}</div>
+                        <div v-else-if="movie.original_name"><strong>Titolo:</strong> {{movie.name}}</div>
+                        <div v-if="movie.title"><strong>Titolo originale:</strong> {{movie.original_title}}</div>
+                        <div v-else-if="movie.original_name"><strong>Titolo originale:</strong> {{movie.original_name}}</div>
+                        <div><strong>Lingua:</strong> <lang-flag :iso="movie.original_language" class="rounded"/></div>
+                        <div><strong>Voto:</strong> {{transformVote(movie.vote_average)}}</div>
+                        <div><i class="fa-solid fa-star"></i></div>
+                    </div>
+            </div>
         </div>
-    <!-- se è una serie -->
-        <div v-else-if="movie.original_name">
-            <ul class="list-unstyled">
-                <li class="mb-3">
-                    <div>{{`Titolo: ${movie.name}`}}</div>
-                    <div>{{`Titolo originale: ${movie.original_name}`}}</div>
-                    <div>Lingua: <lang-flag :iso="movie.original_language" class="rounded"/></div>
-                    <div>{{`Voto: ${movie.vote_average}`}}</div>
-                </li>
-            </ul>
-        </div>
-    </div>
 </template>
 
 <script>
@@ -34,9 +24,41 @@ export default {
     props: {
         movie: Object,
     },
+    methods: {
+        transformVote(vote) {
+            return Math.ceil((vote) / 2);
+        },
+        onImgError(event) {
+            event.target.src = 'https://via.placeholder.com/342x512';
+        },
+        
+    }
 }
 </script>
 
 <style lang="scss">
+.poster {
+    height: 512px;
 
+    img{
+        min-width: 342px;
+        height: 100%;
+    }
+}
+
+.card-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0,0,0,0.8);
+        transition: all .3s ease-in-out;
+        opacity: 0;
+
+        &:hover {
+            opacity: 1;
+            border: 3px solid white;
+        }
+    }
 </style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="flex-grow-1 bg-secondary overflow-auto">
-        <div class="container p-5" v-for="movie in moviesList" :key="movie.id">
-            <StampCard :movie="movie"></StampCard>
+        <div class="container p-5">
+            <div class="d-flex flex-wrap justify-content-center gap-5">
+                <div v-for="movie in moviesList" :key="movie.id" class="d-flex justify-content-start">
+                    <StampCard :movie="movie"></StampCard>
+                </div>
+            </div>
         </div>
     </div>
 </template>
